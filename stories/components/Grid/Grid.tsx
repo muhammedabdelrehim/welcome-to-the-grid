8248f74a5b70c6febf821bbc3608545ec2328649
Muhammed <muhammedabdelrehim@gmail.com>
2019-08-24 14:03:41 +0200

    Grid made. Usestate added with a count.

--- stories/components/Grid/Grid.tsx
+++ stories/components/Grid/Grid.tsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import {} from '../../styles/Grid_styles'
+import React, { useState } from 'react'
+import { GridContainer, GridColumn, GridData } from '../../styles/Grid_styles'
 
 type Props = {
   cells: Number
+  index: Number
 }
 
-const Grid = ({ cells }: Props) => {
-  return <>{cells}</>
+const makeYellow = index => {
+  const [count, setCount] = useState(0)
+  return (
+    <GridData key={index} onClick={() => setCount(count + 1)}>
+      <p>{count}</p>
+    </GridData>
+  )
+}
+
+const createTable = ({ index, cells }: Props) => {
+  let table = []
+
+  // Outer loop to create parent
+  for (let i = 0; i < cells; i++) {
+    let children = []
+    //Inner loop to create children
+    for (let j = 0; j < cells; j++) {
+      children.push(makeYellow(index))
+    }
+    //Create the parent and add the children
+    table.push(<GridColumn>{children}</GridColumn>)
+  }
+  return table
+}
+
+const Grid = index => {
+  return (
+    <>
+      <GridContainer>{createTable(index)}</GridContainer>
+    </>
+  )
 }
 
 Grid.defaultProps = {
--- stories/components/Grid/stories.tsx
+++ stories/components/Grid/stories.tsx
@@ -4,7 +4,7 @@ import { storiesOf } from '@storybook/react'
 import { Grid } from '../Grid'
 
 const Story = () => {
-  return <Grid cells={50} />
+  return <Grid cells={10} />
 }
 
-storiesOf('Components', module).add('Header', () => <Story />)
+storiesOf('Components', module).add('Grid', () => <Story />)
