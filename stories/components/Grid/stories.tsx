ef17510877ccf81de2506b4f35ba1c1391b303e7
Muhammed <muhammedabdelrehim@gmail.com>
2019-08-25 22:46:45 +0200

    Highlighting row and column after click. Start (and most likely ending?) of the Fibonacci series.

--- stories/components/Grid/stories.tsx
+++ stories/components/Grid/stories.tsx
@@ -4,7 +4,7 @@ import { storiesOf } from '@storybook/react'
 import { CreateTable } from '../Grid'
 
 const Story = () => {
-  return <CreateTable cellCount={10} />
+  return <CreateTable cellCount={50} />
 }
 
 storiesOf('Components', module).add('Grid', () => <Story />)
